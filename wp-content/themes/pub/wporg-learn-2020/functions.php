54b339c95e81b421805cbf50e03bd8e113172c78
DanhThong <danhthong@gmail.com>
2022-07-21 20:44:48 +0700

    chore: remove wordpress version

--- wp-content/themes/pub/wporg-learn-2020/functions.php
+++ wp-content/themes/pub/wporg-learn-2020/functions.php
@@ -255,6 +255,7 @@ function wporg_learn_get_lesson_plan_taxonomy_data( $post_id, $context ) {
 		),
 	);
 
+	/*
 	$versions = wporg_learn_get_taxonomy_terms( $post_id, 'wporg_wp_version', $context );
 	if ( $versions ) {
 		$data[] = array(
@@ -264,6 +265,7 @@ function wporg_learn_get_lesson_plan_taxonomy_data( $post_id, $context ) {
 			'value' => $versions,
 		);
 	}
+	*/
 
 	return $data;
 }
